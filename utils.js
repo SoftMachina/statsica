732e9b8a44207632d51cdd12123a2347863e1712
softMachina <kai.k770@live.de>
2023-04-08 12:24:27 +0200

    add sum, mean and variance

--- utils.js
+++ utils.js
@@ -1,1 +1,12 @@
-console.log("statsica");
+
+export const _sum = ((x) => {
+    return x.length === 1 ? x : x.reduce((a, b) => a + b, 0);
+});
+
+export const _mean = ((x) => {
+   return x.length === 1 ? x : _sum(x)/x.length;
+});
+
+export const _var = ((x) => {
+    return x.length === 1 ? x : _sum(x - _mean(x))/x.length;
+});
